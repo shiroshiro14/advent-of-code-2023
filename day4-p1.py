817f4c7018430683374b1a6d1355d4a5405ddb81
Ubuntu <thanhle@leevm.0frdhzk41gkehgqhq4zxxilfdc.ix.internal.cloudapp.net>
2024-01-30 09:57:42 +0000

    Add day 4

---
+++ day4-p1.py
@@ -0,0 +1,27 @@
+import re
+f = open('test.txt', 'r')
+s = f.readlines()
+winning = []
+holding = []
+
+r = 0
+for l in s:
+    temp = l[9:].split('|')
+    winning = temp[0]
+    holding = temp[1]
+
+    wincondi = winning.strip()
+    winnum = re.sub(' +', ' ',wincondi).split(' ')
+    hand = holding.strip()
+    handnum = re.sub(' +', ' ',hand).split(' ')
+    print(wincondi)
+    print(hand)
+    count = 0
+    for num in handnum:
+        if num in winnum:
+            count += 1
+    #print('count:', count)
+    if count > 0:
+        r += pow(2, count - 1)
+print(r)
+
